65a663d0d9e6cc1be0722b59bca4db8c9d14a8bd
versionJun <55729868+versionJun@users.noreply.github.com>
2023-10-07 11:01:15 +0800

    Update demo_datetime.js

--- demo_datetime.js
+++ demo_datetime.js
@@ -1,11 +1,31 @@
-let now = new Date()
+let last_sign_time = new Date('2023-10-06 02:17:32')
 
-console.log(now)
-console.log(now.toUTCString())
-console.log(now.toLocaleString())
+console.log(last_sign_time)
+console.log(last_sign_time.toUTCString())
+console.log(last_sign_time.toLocaleString())
 
-let d = new Date('2023-10-06 02:17:32')
+let now_sign_time = new Date()
 
-console.log(d)
-console.log(d.toUTCString())
-console.log(d.toLocaleString())
+console.log(now_sign_time)
+console.log(now_sign_time.toUTCString())
+console.log(now_sign_time.toLocaleString())
+
+
+
+
+let timestamp_1 = Date.parse(last_sign_time)
+let timestamp_2 = Date.parse(now_sign_time)
+
+console.log()
+
+if (
+    (timestamp_2 - timestamp_1) > (24 * 60 * 60 * 1000)
+    ) {
+
+        console.log(`距上次签到时间大于24小时啦,可签到(上次签到时间:${last_sign_time})(本次触发时间:${now_sign_time})`)
+    
+} else {    
+
+    console.log(`---还未到时间！(上次签到时间:${last_sign_time})(本次触发时间:${now_sign_time})`)
+
+}
