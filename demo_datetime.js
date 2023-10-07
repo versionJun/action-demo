a01b537c96d43fa579515fa2c4e42ff8dd0cb422
versionJun <55729868+versionJun@users.noreply.github.com>
2023-10-07 10:50:00 +0800

    Create demo_datetime.js

---
+++ demo_datetime.js
@@ -0,0 +1,11 @@
+let now = new Date()
+
+console.log(now)
+console.log(now.toUTCString())
+console.log(now.toLocaleString())
+
+let d = new Date('2023-10-06 02:17:32')
+
+console.log(d)
+console.log(d.toUTCString())
+console.log(d.toLocaleString())
