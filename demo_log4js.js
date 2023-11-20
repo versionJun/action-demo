7ad848689dd07fe03da0fd6721e3bc9307a1a1c4
versionJun <55729868+versionJun@users.noreply.github.com>
2023-11-20 12:48:35 +0800

    Create demo_log4js.js

---
+++ demo_log4js.js
@@ -0,0 +1,71 @@
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.tz.setDefault('Asia/Shanghai')
+const log4js = require("log4js");
+const recording = require('log4js/lib/appenders/recording');
+
+log4js.addLayout("div", function (config) {
+  return function (logEvent) {
+    return log4jsLayoutDiv(logEvent, config)
+  };
+});
+
+function log4jsLayoutDiv(log4jsEvent, log4jsConfig){
+  return `${dayjs(log4jsEvent.startTime).format('YYYY-MM-DD HH:mm:ss')} ${log4jsEvent.data.join('')}`    
+}
+
+log4js.configure({
+  appenders: {
+    recording: {
+        type: 'recording',
+    },
+    stdout: { 
+        type: "stdout",
+        layout: { 
+          type: "div",
+          // separator: "," 
+        }
+    },
+  },
+  categories: {
+    default: { 
+        appenders: [
+            "recording", 
+            "stdout"
+        ], 
+        level: "trace" 
+    },
+  },
+});
+
+const logger = log4js.getLogger("demo_log4js")
+
+!(async () => {
+  logger.trace("Entering cheese testing");
+  await sleep(1000)
+  logger.debug("Got cheese.");
+  await sleep(1000)
+  logger.info("Cheese is Comté.");
+  await sleep(1000)
+  logger.warn("Cheese is quite smelly.");
+  await sleep(1000)
+  logger.error("Cheese is too ripe!");
+  await sleep(1000)
+  logger.fatal("Cheese was breeding ground for listeria.");
+  await sleep(1000)
+
+
+  recording.replay().map((e) => {
+    console.log(e)
+  })
+})()
+
+
+async function sleep(duration) {
+  return new Promise(resolve => {
+      setTimeout(resolve, duration);
+  })
+}
