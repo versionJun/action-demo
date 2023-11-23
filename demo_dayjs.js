4b35fe16225e45105936c4f77ecd427cfdf4e36d
versionJun <55729868+versionJun@users.noreply.github.com>
2023-11-23 20:27:37 +0800

    Update demo_dayjs.js

--- demo_dayjs.js
+++ demo_dayjs.js
@@ -5,7 +5,10 @@ dayjs.extend(utc)
 dayjs.extend(timezone)
 dayjs.tz.setDefault('Asia/Shanghai')
 
+
+console.log(dayjs.tz.guess())
+
 const d = '2023-11-21T16:57:01.338+00:00'
 console.log(`${dayjs(d).format('YYYY-MM-DD HH:mm:ss')}`)
 console.log(`${dayjs.tz(d).format('YYYY-MM-DD HH:mm:ss')}`)
-console.log(`${dayjs.tz(dayjs(d).format('YYYY-MM-DD HH:mm:ss')).format('YYYY-MM-DD HH:mm:ss')}`)
+console.log(`${dayjs.tz(dayjs(d)).format('YYYY-MM-DD HH:mm:ss')}`)
