863882cb77af10dbbbb8bc4830d89d29cc85afec
versionJun <55729868+versionJun@users.noreply.github.com>
2023-11-23 21:31:50 +0800

    Update demo_dayjs.js

--- demo_dayjs.js
+++ demo_dayjs.js
@@ -12,3 +12,7 @@ const d = '2023-11-21T16:57:01.338+00:00'
 console.log(`${dayjs(d).format('YYYY-MM-DD HH:mm:ss')}`)
 console.log(`${dayjs.tz(d).format('YYYY-MM-DD HH:mm:ss')}`)
 console.log(`${dayjs.tz(dayjs(d)).format('YYYY-MM-DD HH:mm:ss')}`)
+
+
+const IS_SCHEDULE = process.env.IS_SCHEDULE 
+console.log(IS_SCHEDULE)
