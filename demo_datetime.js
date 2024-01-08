b7a86326cbbe4c491e788867db4dc2f0ba03d4c0
versionJun <55729868+versionJun@users.noreply.github.com>
2024-01-09 04:25:59 +0800

    Update demo_datetime.js

--- demo_datetime.js
+++ demo_datetime.js
@@ -1,18 +1,21 @@
-let last_sign_time = new Date('2023-10-06 02:17:32')
+// let last_sign_time = new Date('2023-10-06 02:17:32')
 
-console.log(last_sign_time)
-console.log(last_sign_time.toUTCString())
-console.log(last_sign_time.toLocaleString())
+// console.log(last_sign_time)
+// console.log(last_sign_time.toUTCString())
+// console.log(last_sign_time.toLocaleString())
 
-let now_sign_time = new Date(new Date().getTime() + 28800000)
+// let now_sign_time = new Date(new Date().getTime() + 28800000)
 
 
-console.log(now_sign_time)
-console.log(now_sign_time.toUTCString())
-console.log(now_sign_time.toLocaleString())
+// console.log(now_sign_time)
+// console.log(now_sign_time.toUTCString())
+// console.log(now_sign_time.toLocaleString())
 
 
 
 
 console.log(Intl.DateTimeFormat().resolvedOptions().timeZone)
+
 console.log(new Date().getTimezoneOffset() / 60)
+
+console.log((new Date())
